a760860b52bd7fbc8795b559e5d2051a048549fa
AlinaAbaieva <alina.abaieva@privatbank.ua>
2021-10-15 12:35:18 +0300

    JavaHomeWork hw2.1.1 calc+interval

--- src/com/pb/abaieva/hw2/Interval.java
+++ src/com/pb/abaieva/hw2/Interval.java
@@ -14,11 +14,11 @@ public class Interval {
         if ((numChosen > -1) && (numChosen < 15)) {
             intervalChosen = "Интервал числа " + numChosen + " - [0-14].";
         } else if ((numChosen > 14) && (numChosen < 36)) {
-            intervalChosen = "Интервал числа " + numChosen + " - [15-35]";
+            intervalChosen = "Интервал числа " + numChosen + " - [15-35].";
         } else if ((numChosen > 35) && (numChosen < 51)) {
-            intervalChosen = "Интервал числа " + numChosen + " - [36-50]";
+            intervalChosen = "Интервал числа " + numChosen + " - [36-50].";
         } else if ((numChosen > 50) && (numChosen < 101)) {
-            intervalChosen = "Интервал числа " + numChosen + " - [51-100]";
+            intervalChosen = "Интервал числа " + numChosen + " - [51-100].";
         } else intervalChosen =
                 "Ошибка! Число не принадлежит допустимым интервалам: [0-14], [15-35], [36-50], [51-100].";
         System.out.println(intervalChosen);
