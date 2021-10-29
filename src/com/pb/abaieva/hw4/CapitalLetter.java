df71bc6eb521e442a7511bc8139465b713c38056
AlinaAbaieva <alina.abaieva@privatbank.ua>
2021-10-29 16:23:17 +0300

    JavaHomeWork hw4.1 Capital! comms

--- src/com/pb/abaieva/hw4/CapitalLetter.java
+++ src/com/pb/abaieva/hw4/CapitalLetter.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CapitalLetter {
 
+    //запрашивает ввод строки
     static String requestInput() {
         Scanner scanLine = new Scanner(System.in);
         System.out.println("Введите строку: ");
@@ -12,7 +13,6 @@ public class CapitalLetter {
 
     static StringBuilder upFirstLetters(String string) {
         char[] parsedString = string.toCharArray();
-
         String[] upperString = new String[parsedString.length];
 
         StringBuilder finalString = new StringBuilder();
@@ -20,9 +20,12 @@ public class CapitalLetter {
         for (int i = 0; i < parsedString.length; i++) {
             if ((i == 0) || (parsedString[i - 1] == ' ')) {
                 upperString[i] = String.valueOf((parsedString[i])).toUpperCase();
+                    //если символ первый в строке или первый после пробела - переводит в строку и делает заглавным
             } else {
                 upperString[i] = String.valueOf(parsedString[i]);
+                    //иначе просто переводит символ в строку
             }
+
             finalString.append(upperString[i]);
         }
 
@@ -39,5 +42,4 @@ public class CapitalLetter {
         System.out.println(outputString);
 
     }
-
 }
